b395317e81ad23dc5e1692309c87407ed60e2c3e
LiekeDeckers <liekedeckers15@gmail.com>
2023-03-22 11:48:45 +0100

    #8

--- lib/gerecht_info.php
+++ lib/gerecht_info.php
@@ -16,18 +16,19 @@ class gerecht_info {
         return($data_usr);
     }
 
+// user ophalen bij opmerking en favoriet    
     public function selecteerGerechtInfo($gerecht_id, $record_type) {
         $sql = "select * from gerecht_info where gerecht_id = $gerecht_id and record_type = '$record_type'";
-        echo $sql;
+        //echo $sql;
         $result = mysqli_query($this->connection, $sql);
 
         while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
             if ($record_type == 'O' || $record_type == 'F') { 
-                var_dump($row);
+                //var_dump($row);
                 $user_id = $row['user_id'];
-                echo $user_id;
+                //echo $user_id;
                 $user = $this->ophalenUser($user_id);
-                var_dump($user);
+                //var_dump($user);
             
             $gerecht_info[] = [
                 'gerecht_info_id' => $row['gerecht_info_id'],
@@ -47,5 +48,20 @@ class gerecht_info {
 
         return($gerecht_info);    
         }
-    }    
+    }
+    
+// aan favorieten toevoegen/verwijderen
+    public function toevoegenFavoriet($gerecht_id, $user_id) {
+        if($record_type == 'F', $gerecht_id == false, $user_id == false) {
+        $sql = "INSERT INTO gerecht_info ('record_type', 'gerecht_id', 'user_id')
+        VALUES('F', $gerecht_id, $user_id)";
+        echo "Toegevoegd aan favorieten";
+        }
+
+        elseif($record_type['F'], $gerecht_id == true, $user_id == true) {
+            $sql = "DELETE FROM gerecht_info where gerecht_id = $gerecht_id and record_type = 'F'";
+            echo "Verwijderd uit favorieten";
+
+    }
+
 }
