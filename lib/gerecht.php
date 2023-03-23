2e28ee1a883a98459ddbc2ca5e3a7e06f7b537f7
LiekeDeckers <liekedeckers15@gmail.com>
2023-03-23 17:06:00 +0100

    ophalen user, keuken, opmerkingen enz. #10

--- lib/gerecht.php
+++ lib/gerecht.php
@@ -31,8 +31,8 @@ class gerecht {
         return($data_tk);
     }
 
-    private function ophalenIngredient($ingredient_id) {
-        $data_ing = $this->ing->selecteerIngredient($ingredient_id);
+    private function ophalenIngredient($gerecht_id) {
+        $data_ing = $this->ing->selecteerIngredient($gerecht_id);
         return($data_ing);
     }
 
@@ -42,40 +42,79 @@ class gerecht {
     }
 
     private function ophalenWaardering($gerecht_id) {
-        return $this->ger_info->selecteerGerechtInfo($gerecht_info_id, 'W');
+        return $this->gerinfo->selecteerGerechtInfo($gerecht_id, 'W');
     }
 
     private function ophalenFavoriet($gerecht_id) {
-        return $this->ger_info->selecteerGerechtInfo($gerecht_info_id, 'F');
+        return $this->gerinfo->selecteerGerechtInfo($gerecht_id, 'F');
     }
 
     private function ophalenOpmerking($gerecht_id) {
-        return $this->ger_info->selecteerGerechtInfo($gerecht_info_id, 'O');
+        return $this->gerinfo->selecteerGerechtInfo($gerecht_id, 'O');
     }
 
     private function ophalenBereiding($gerecht_id) {
-        return $this->ger_info->selecteerGerechtInfo($gerecht_info_id, 'B');
+        return $this->gerinfo->selecteerGerechtInfo($gerecht_id, 'B');
     }
+
+
 
 //ophalen user
     public function selecteerGerecht($gerecht_id) {
+        $gerecht_data = [
+            "id" => "",
+            "keuken" => [],
+            "type" => [],
+            "user" => [],
+            "datum_toegevoegd" => "",
+            "titel" => "",
+            "korte_omschrijving" => "",
+            "lange_omschrijving" => "",
+            "afbeelding" => "",
+            "ingredienten" => [],
+            "favoriet" => [],
+            "waardering" => [],
+            "bereidingswijze" => [],
+            "opmerkingen" => [],
+            "prijs_gerecht" => "",
+            "calorieen" => "",
+            "gemiddelde_waardering" => ""
+        ];
+
+
         $sql = "select * from gerecht where gerecht_id = $gerecht_id";
         $result = mysqli_query($this->connection, $sql);
 
-        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-            $user_id = $row['user_id'];
-            $user = $this->ophalenUser($user_id);
-            
-//ophalen ingredient
-            $gerecht_id = $row['gerecht_id'];
-            $gerecht = $this->ophalenIngredient($gerecht_id);
+        $kaalGerechtData = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
-            $gerecht[] = $row + $user;
-        }
-        return($gerecht);
+        $gerecht_data["id"] = $kaalGerechtData["gerecht_id"];
+        $gerecht_data["keuken"] = $this->ophalenTypeKeuken($kaalGerechtData["keuken_id"]);
+        $gerecht_data["type"] = $this->ophalenTypeKeuken($kaalGerechtData["type_id"]);
+        $gerecht_data["user"] = $this->ophalenUser($kaalGerechtData["user_id"]);
+        $gerecht_data["datum_toegevoegd"] = $kaalGerechtData["datum_toegevoegd"];
+        $gerecht_data["titel"] = $kaalGerechtData["titel"];
+        $gerecht_data["korte_omschrijving"] = $kaalGerechtData["korte_omschrijving"];
+        $gerecht_data["lange_omschrijving"] = $kaalGerechtData["lange_omschrijving"];
+        $gerecht_data["ingredienten"] = $this->ophalenIngredient($kaalGerechtData["gerecht_id"]);
+        $gerecht_data["favoriet"] = $this->ophalenFavoriet($kaalGerechtData["gerecht_id"]);
+        $gerecht_data["waardering"] = $this->ophalenWaardering($kaalGerechtData["gerecht_id"]);
+        $gerecht_data["bereidingswijze"] = $this->ophalenBereiding($kaalGerechtData["gerecht_id"]);
+        $gerecht_data["opmerkingen"] = $this->ophalenOpmerking($kaalGerechtData["gerecht_id"]);
+        //$gerecht_data["totaalprijs"] = $this->berekenPrijs($gerecht_data["ingredienten"]);
+        //$gerecht_data["calorieen"] = $this->berekenCalorieen($gerecht_data["ingredienten"]);
+        //$gerecht_data["gemiddelde_waardering"] = $this->berekenGemiddeldeWaardering($gerecht_data["waardering"]);
+
+        return $gerecht_data;
     }
+} 
+
+        
 
 
+
+        
+
+/*
 //berekenen calorieen
     private function berekenCalorieen($gerecht_id) {
         $calorieen_gerecht = [];
@@ -152,3 +191,4 @@ class gerecht {
 //ophalen keuken
 //ophalen type
 //maak favoriet 
+*/
