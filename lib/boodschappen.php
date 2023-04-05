a740277a43e2d06c0205b769eee6909ac5665d5c
LiekeDeckers <liekedeckers15@gmail.com>
2023-04-05 14:26:22 +0200

    aantal en prijs totaal boodschappenlijst

--- lib/boodschappen.php
+++ lib/boodschappen.php
@@ -44,19 +44,21 @@ class boodschappen {
             $artikel_id = $row['artikel_id'];
             $artikel = $this->ophalenArtikel($artikel_id);
 
+            $ingredient_id = $row['ingredient_id'];
+            $ingredient = $this->ophalenIngredient($ingredient_id);
+
             $boodschappen[] = [
                 'lijst_id' => $row['lijst_id'],
                 'user_id' => $row['user_id'],
                 'ingredient_id' => $row['ingredient_id'],
-                'gebruikt' => $row['gebruikt'],
-                'aantal_kopen' => $row['aantal_kopen'],
                 'naam' => $artikel['naam'],
                 'omschrijving' => $artikel['omschrijving'],
                 'prijs' => $artikel['prijs'],
                 'eenheid' => $artikel['eenheid'],
                 'verpakking' => $artikel['verpakking'],
                 'calorieen' => $artikel['calorieen'],
-                'artikel_afbeelding' => $artikel['artikel_afbeelding']
+                'artikel_afbeelding' => $artikel['artikel_afbeelding'],
+                'aantal_kopen' => $this->berekenAantal($ingredient),
             ];        
         }
 
@@ -69,26 +71,18 @@ class boodschappen {
 
     public function toevoegenBoodschappen($gerecht_id, $user_id) {
 
-        $data_ing = $this->ing->selecteerIngredient($gerecht_id);
-
+        $ingredient = $this->selecteerIngredient($gerecht_id);
+        
         foreach($ingredienten as $ingredient) {
-            $gebruikt = $ingredient["aantal"] / $ingredient["verpakking"];
-
-            // al op lijst
-            if(artikelOpLijst != false) {            
-            $gebruikt += $boodschappenljst['gebruikt'];
-
-            $sql = "UPDATE boodschappenljst SET aantal_kopen = " . ceil($gebruikt) . ", gebruikt = $gebruikt 
-            WHERE user_id = $user_id AND artikel_id = " . $ingredient["artikel_id"]; 
+            $opgehaald = $this->artikelOpLijst($artikel_id, $user_id);
+            if(!$opgehaald) {
+                $this->toevoegenArtikel($artikel, $user_id);
             }
-
-            // nog niet op lijst
+        
             else {
-            $sql = "INSERT INTO boodschappenljst(user_id, artikel_id, gebruikt, aantal_kopen) VALUES
-            VALUES ('$user_id', '$artikel_id', '$gebruikt', 'ceil($gebruikt)')";
+                $this->artikelBijwerken($opgehaald, $artikel);
             }
         }
-
     }
 
 
@@ -114,31 +108,14 @@ class boodschappen {
     }
 
 // aantal berekenen
-    private function berekenAantal($boodschappen, $ingredient) {
+    private function berekenAantal($ingredient) {
         $aantal_kopen = 0;
 
-        $aantal_kopen = ceil($aantal_kopen + ($ingredient["aantal"] / $$ingredient["verpakking"]));
-
-        return($aantal_kopen);
-    }
-
-// prijs totaal berekenen
-    public function totaalPrijsBoodschappen($user_id) {
-        
-        $sql = "select * drom boodschappenljst where user_id = $user_id";
-        $result = mysqli_query($this->connection, $sql);
-
-        $prijs_totaal = 0;
-
-        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-            $artikel_id = $row["artikel_id"];
-            $artikel = $this->selectArtikel($artikel_id);
-
-            $prijs_totaal = $prijs_totaal + ($artikel["prijs"] * $boodschappen["aantal_kopen"]);
+        foreach($ingredient as $ingredient) {
+        $aantal_kopen += ($ingredient["aantal"] / $ingredient["verpakking"]);
         }
 
-        return ($prijs_totaal);
-    
-}
+        return ceil($aantal_kopen);
+    }
 
 } 
